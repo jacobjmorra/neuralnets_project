8c94745969be2a7a85d05c33a19d1970d3c21ab2
Anthony Cruz <anthonycruz202@gmail.com>
2021-02-18 17:06:59 -0500

    Update anthony_code.py
    Added synapses with simple weights, removed plot of I vs. FR, and changed some variable names.

--- single_neuron/anthony_code.py
+++ single_neuron/anthony_code.py
@@ -9,11 +9,17 @@ def hh(duration = 2*second, plots = True):
     El, EK, ENa = 10.6*mV, -12*mV, 115*mV # Potentials, El is resting
     VT = -63*mV
 
+    w_e, w_i = .1*msiemens, .4*msiemens
+    Ee, Ei = 0*mV, -80*mV
+    taue, taui = 5*ms, 10*ms
+
     eqs = Equations('''
-    dv/dt = (g_na*(m**3)*h*(ENa-v) + g_kd*(n**4)*(EK-v) + gl*(El-v) + I)/Cm : volt
+    dv/dt = (g_na*(m**3)*h*(ENa-v) + g_kd*(n**4)*(EK-v) + gl*(El-v) + ge*(Ee-v) + gi*(Ei-v) + Iext)/Cm : volt
     dm/dt = (Am*v*(1-m) - Bm*v*m)/volt: 1
     dn/dt = (An*v*(1-m) - Bn*v*m)/volt : 1
     dh/dt = (Ah*v*(1-m) - Bh*v*m)/volt : 1
+    dge/dt = -ge/taue : siemens
+    dgi/dt = -gi/taui : siemens
 
     An = ((10*mV - v)/volt) / ((100*exp( ((10*mV - v)/10)/volt )-1)*ms) : Hz
     Bn = .125 * exp(-v/(80*volt)) / ms : Hz
@@ -21,20 +27,25 @@ def hh(duration = 2*second, plots = True):
     Bm = 4 * exp(-v/(18*volt)) / ms : Hz
     Ah = .07 * exp(-v/(20*volt)) / ms : Hz
     Bh = 1 / ((exp( ((30*mV - v)/10)/volt )+1)*ms) : Hz
-    I : amp
+    Iext : amp
     ''')
 
     group = NeuronGroup(3, eqs, threshold='v > -40*mV', refractory='v > -40*mV', method='exponential_euler')
-    group.v = El; group.I = '.7*nA * i/3'
-    spikes = SpikeMonitor(group); M = StateMonitor(group, 'v', record = 0)
-    S = StateMonitor(group, 'v', record = 1); I_m = StateMonitor(group, 'v', record = 2)
+    group.v = El; group.Iext = '28e-11*amp * i / 3'
+
+    # Creating spike monitor, voltage monitors, and synapses
+    spikes = SpikeMonitor(group)
+    M = StateMonitor(group, 'v', record = 0) # Master
+    S = StateMonitor(group, 'v', record = 1) # Slave
+    I = StateMonitor(group, 'v', record = 2) # Inter
+    MSSI = Synapses(group, group, 'w: siemens', on_pre='ge += w_e'); MSSI.connect(i = [0,1], j = [1, 2])
+    IS = Synapses(group, group, 'w: siemens', on_pre='gi += w_i'); IS.connect(i = [2], j = [1])
     run(duration)
 
     if plots:
-        subplot(1,2,1); plot(group.I/nA, spikes.count / duration, "k.-", linewidth = .5) # Current vs. FR, 1 datum per neuron
-        xlabel('Input Current (nA)'); ylabel('Firing rate (Hz)')
-        subplot(1,2,2); plot(M.t/ms, M.v[0], "g--", linewidth = .5)
-        plot(S.t/ms, S.v[0], "r--", linewidth = .5); plot(I_m.t/ms, I_m.v[0], "b--", linewidth = .5)
-        xlabel('Time (ms)'); ylabel('Voltage (mV)')
-        subplots_adjust(wspace=.5); show()
-hh(10*second)
+        plot(M.t/ms, M.v[0], "g--", linewidth = .5)
+        plot(S.t/ms, S.v[0], "r--", linewidth = .5)
+        plot(I.t/ms, I.v[0], "b--", linewidth = .5)
+        xlabel('Time (ms)'); ylabel('Voltage (mV)'); show()
+    return(spikes.count/duration)
+print(hh(2*second))
