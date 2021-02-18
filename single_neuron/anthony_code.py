b9f7e6dcdb36c014abf0790fe84b22ad8f2ee77e
Anthony Cruz <anthonycruz202@gmail.com>
2021-02-18 00:21:03 -0500

    Update anthony_code.py
    Added paper parameter values to sample HH function.

--- single_neuron/anthony_code.py
+++ single_neuron/anthony_code.py
@@ -2,18 +2,17 @@ from brian2 import *
 
 # https://brian2.readthedocs.io/en/stable/examples/IF_curve_Hodgkin_Huxley.html
 
-def hh_fire(duration = 2*second):
-    # Parameters
+def hh_fire(duration = 2*second, plots = True):
+    start_scope()
     area = 20000*umetre**2
-    Cm = 1*ufarad*cm**-2 * area
-    gl = 5e-5*siemens*cm**-2 * area
-    El, EK, ENa = -65*mV, -90*mV, 50*mV # Potentials
-    g_na, g_kd = 100*msiemens*cm**-2 * area, 30*msiemens*cm**-2 * area # Conductances
+    Cm = 9*pi*ufarad*cm**-2 * area
+    gl = 2.7*pi*msiemens*cm**-2 * area
+    g_na, g_kd = 1080*pi*msiemens*cm**-2 * area, 324*pi*msiemens*cm**-2 * area # Conductances
+    El, EK, ENa = 10.6*mV, -12*mV, 115*mV # Potentials, El is resting
     VT = -63*mV
 
-    # The model
     eqs = Equations('''
-    dv/dt = (gl*(El-v) - g_na*(m*m*m)*h*(v-ENa) - g_kd*(n*n*n*n)*(v-EK) + I)/Cm : volt
+    dv/dt = (g_na*(m**3)*h*(ENa-v) + g_kd*(n**4)*(EK-v) + gl*(El-v) + I)/Cm : volt
     dm/dt = 0.32*(mV**-1)*4*mV/exprel((13.*mV-v+VT)/(4*mV))/ms*(1-m)-0.28*(mV**-1)*5*mV/exprel((v-VT-40.*mV)/(5*mV))/ms*m : 1
     dn/dt = 0.032*(mV**-1)*5*mV/exprel((15.*mV-v+VT)/(5*mV))/ms*(1.-n)-.5*exp((10.*mV-v+VT)/(40.*mV))/ms*n : 1
     dh/dt = 0.128*exp((17.*mV-v+VT)/(18.*mV))/ms*(1.-h)-4./(1+exp((40.*mV-v+VT)/(5.*mV)))/ms*h : 1
@@ -21,16 +20,16 @@ def hh_fire(duration = 2*second):
     ''')
 
     # Threshold and refractoriness are only used for spike counting
-    group = NeuronGroup(3, eqs,
-                        threshold='v > -40*mV',
-                        refractory='v > -40*mV',
-                        method='exponential_euler')
-    group.v = El
-    group.I = '.7*nA * i / 3'
-
-    monitor = SpikeMonitor(group)
-
+    group = NeuronGroup(3, eqs, threshold='v > -40*mV', refractory='v > -40*mV', method='exponential_euler')
+    group.v = El; group.I = '.7*nA * i / 3'
+    S = SpikeMonitor(group); M = StateMonitor(group, 'v', record = 0)
     run(duration)
-    plot(group.I/nA, monitor.count / duration)
-    xlabel('I (nA)'); ylabel('Firing rate (Hz)'); show()
-hh_fire()
+
+    if plots:
+        subplot(1,2,1); plot(group.I/nA, S.count / duration, "k.-", linewidth = .5) # Current vs. FR, 1 datum per neuron
+        xlabel('Input Current (nA)'); ylabel('Firing rate (Hz)')
+        subplot(1,2,2); plot(M.t/ms, M.v[0], "k-", linewidth = .5)
+        xlabel('Time (ms)'); ylabel('Voltage (mV)')
+        subplots_adjust(wspace=.5); show()
+
+hh_fire(2*second)
