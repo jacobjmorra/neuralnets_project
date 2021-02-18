baa5da947c8b3f8ced4e60a4a0c2da3e19d67ad0
Anthony Cruz <anthonycruz202@gmail.com>
2021-02-18 10:50:07 -0500

    Update anthony_code.py
    Got alpha_m equation working and added filler for dx/dt. Will add others soon.

--- single_neuron/anthony_code.py
+++ single_neuron/anthony_code.py
@@ -1,25 +1,27 @@
+# https://brian2.readthedocs.io/en/stable/examples/IF_curve_Hodgkin_Huxley.html
 from brian2 import *
 
-# https://brian2.readthedocs.io/en/stable/examples/IF_curve_Hodgkin_Huxley.html
-
-def hh_fire(duration = 2*second, plots = True):
+def hh(duration = 2*second, plots = True):
     start_scope()
-    area = 20000*umetre**2
-    Cm = 9*pi*ufarad*cm**-2 * area
-    gl = 2.7*pi*msiemens*cm**-2 * area
-    g_na, g_kd = 1080*pi*msiemens*cm**-2 * area, 324*pi*msiemens*cm**-2 * area # Conductances
+    Cm = 9*pi*ufarad
+    gl = 2.7*pi*msiemens
+    g_na, g_kd = 1080*pi*msiemens, 324*pi*msiemens # Conductances
     El, EK, ENa = 10.6*mV, -12*mV, 115*mV # Potentials, El is resting
     VT = -63*mV
+    Bm = 1/second
+    An, Bn = 1/second, 1/second
+    Ah, Bh = 1/second, 1/second
 
     eqs = Equations('''
     dv/dt = (g_na*(m**3)*h*(ENa-v) + g_kd*(n**4)*(EK-v) + gl*(El-v) + I)/Cm : volt
-    dm/dt = 0.32*(mV**-1)*4*mV/exprel((13.*mV-v+VT)/(4*mV))/ms*(1-m)-0.28*(mV**-1)*5*mV/exprel((v-VT-40.*mV)/(5*mV))/ms*m : 1
-    dn/dt = 0.032*(mV**-1)*5*mV/exprel((15.*mV-v+VT)/(5*mV))/ms*(1.-n)-.5*exp((10.*mV-v+VT)/(40.*mV))/ms*n : 1
-    dh/dt = 0.128*exp((17.*mV-v+VT)/(18.*mV))/ms*(1.-h)-4./(1+exp((40.*mV-v+VT)/(5.*mV)))/ms*h : 1
+    dm/dt = (Am*v*(1-m) - Bm*v*m)/volt: 1
+    dn/dt = (An*v*(1-m) - Bn*v*m)/volt : 1
+    dh/dt = (Ah*v*(1-m) - Bh*v*m)/volt : 1
+
+    Am = ((25*mV - v)/volt) / ((100*exp( ((25*mV - v)/10)/volt )-1)*ms) : Hz
     I : amp
     ''')
 
-    # Threshold and refractoriness are only used for spike counting
     group = NeuronGroup(3, eqs, threshold='v > -40*mV', refractory='v > -40*mV', method='exponential_euler')
     group.v = El; group.I = '.7*nA * i / 3'
     S = SpikeMonitor(group); M = StateMonitor(group, 'v', record = 0)
@@ -31,5 +33,4 @@ def hh_fire(duration = 2*second, plots = True):
         subplot(1,2,2); plot(M.t/ms, M.v[0], "k-", linewidth = .5)
         xlabel('Time (ms)'); ylabel('Voltage (mV)')
         subplots_adjust(wspace=.5); show()
-
-hh_fire(2*second)
+hh(2*second)
