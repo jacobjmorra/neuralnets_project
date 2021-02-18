727e36ffe0026d15b81797a6f3c4f05389f30b61
Anthony Cruz <anthonycruz202@gmail.com>
2021-02-17 23:15:38 -0500

    Update anthony_code.py
    Basic HH function.

--- single_neuron/anthony_code.py
+++ single_neuron/anthony_code.py
@@ -1,30 +1,36 @@
-# Auditory stimuli are modelled as step currents.
-# Frequencies in range [.5,32]kHz.
-# 100ms in length, 100ms offset --> 200ms rhythm.
-
-# Next step: Need to configure the step input properly.
 from brian2 import *
 
-def pl_lif(pf = 5000*Hz, f = 10*Hz, runtime = 1000*ms, plot_spikes = True):
-    start_scope()
-    tau = 20*ms
-    Vr, Vth, Vrest = -70*mV, -54*mV, -60*mV # Voltages reset/threshold/rest
-    I, R = 10*nA, 1/(1000*nS) # Input current and resistance, not specified in paper, except that input is a step current.
-    eqs = '''
-    dv/dt = (-(v-Vrest) + I*R + A*sin(2*pi*f*t + b)) / tau : volt
-    db/dt = abs(pf - f)/(31500*Hz*2*200*ms) : 1
-    A = 10*mV : volt # Oscillation Amplitude, kept constant
-    '''
-    neuron = NeuronGroup(1, model=eqs, threshold='v>Vth', reset='v=Vr', refractory=5*ms, method='euler')
-    neuron.v = Vrest; neuron.b = 0
-    S = SpikeMonitor(neuron)
-    trace = StateMonitor(neuron, 'v', record = True)
+# https://brian2.readthedocs.io/en/stable/examples/IF_curve_Hodgkin_Huxley.html
 
-    run(runtime - (runtime - 500*ms))
-    I = 0*nA; run(runtime - 500*ms)
-    if plot_spikes:
-        plot(trace.t/ms, trace.v.T); plot(S.t/ms, [Vth]*S.num_spikes, "ro"); xlabel('Time (ms)'); ylabel('Voltage (V)')
-        tight_layout(); show()
-    return(S.num_spikes / runtime)
+def hh_fire(duration = 2*second):
+    # Parameters
+    area = 20000*umetre**2
+    Cm = 1*ufarad*cm**-2 * area
+    gl = 5e-5*siemens*cm**-2 * area
+    El, EK, ENa = -65*mV, -90*mV, 50*mV # Potentials
+    g_na, g_kd = 100*msiemens*cm**-2 * area, 30*msiemens*cm**-2 * area # Conductances
+    VT = -63*mV
 
-pl_lif(pf = 5000*Hz, f = 10*Hz, runtime = 2000*ms)
+    # The model
+    eqs = Equations('''
+    dv/dt = (gl*(El-v) - g_na*(m*m*m)*h*(v-ENa) - g_kd*(n*n*n*n)*(v-EK) + I)/Cm : volt
+    dm/dt = 0.32*(mV**-1)*4*mV/exprel((13.*mV-v+VT)/(4*mV))/ms*(1-m)-0.28*(mV**-1)*5*mV/exprel((v-VT-40.*mV)/(5*mV))/ms*m : 1
+    dn/dt = 0.032*(mV**-1)*5*mV/exprel((15.*mV-v+VT)/(5*mV))/ms*(1.-n)-.5*exp((10.*mV-v+VT)/(40.*mV))/ms*n : 1
+    dh/dt = 0.128*exp((17.*mV-v+VT)/(18.*mV))/ms*(1.-h)-4./(1+exp((40.*mV-v+VT)/(5.*mV)))/ms*h : 1
+    I : amp
+    ''')
+
+    # Threshold and refractoriness are only used for spike counting
+    group = NeuronGroup(3, eqs,
+                        threshold='v > -40*mV',
+                        refractory='v > -40*mV',
+                        method='exponential_euler')
+    group.v = El
+    group.I = '.7*nA * i / 3'
+
+    monitor = SpikeMonitor(group)
+
+    run(duration)
+    plot(group.I/nA, monitor.count / duration)
+    xlabel('I (nA)'); ylabel('Firing rate (Hz)'); show()
+hh_fire()
