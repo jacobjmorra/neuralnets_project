a0d7e7a6766ce3672abe8b449e631262e5695495
Anthony Cruz <anthonycruz202@gmail.com>
2021-02-18 15:49:27 -0500

    Update anthony_code.py
    Added all alpha and beta equations.

--- single_neuron/anthony_code.py
+++ single_neuron/anthony_code.py
@@ -8,9 +8,6 @@ def hh(duration = 2*second, plots = True):
     g_na, g_kd = 1080*pi*msiemens, 324*pi*msiemens # Conductances
     El, EK, ENa = 10.6*mV, -12*mV, 115*mV # Potentials, El is resting
     VT = -63*mV
-    Bm = 1/second
-    An, Bn = 1/second, 1/second
-    Ah, Bh = 1/second, 1/second
 
     eqs = Equations('''
     dv/dt = (g_na*(m**3)*h*(ENa-v) + g_kd*(n**4)*(EK-v) + gl*(El-v) + I)/Cm : volt
@@ -18,12 +15,17 @@ def hh(duration = 2*second, plots = True):
     dn/dt = (An*v*(1-m) - Bn*v*m)/volt : 1
     dh/dt = (Ah*v*(1-m) - Bh*v*m)/volt : 1
 
-    Am = ((25*mV - v)/volt) / ((100*exp( ((25*mV - v)/10)/volt )-1)*ms) : Hz
+    An = ((10*mV - v)/volt) / ((100*exp( ((10*mV - v)/10)/volt )-1)*ms) : Hz
+    Bn = .125 * exp(-v/(80*volt)) / ms : Hz
+    Am = ((25*mV - v)/volt) / ((10*exp( ((25*mV - v)/10)/volt )-1)*ms) : Hz
+    Bm = 4 * exp(-v/(18*volt)) / ms : Hz
+    Ah = .07 * exp(-v/(20*volt)) / ms : Hz
+    Bh = 1 / ((exp( ((30*mV - v)/10)/volt )+1)*ms) : Hz
     I : amp
     ''')
 
     group = NeuronGroup(3, eqs, threshold='v > -40*mV', refractory='v > -40*mV', method='exponential_euler')
-    group.v = El; group.I = '.7*nA * i / 3'
+    group.v = El; group.I = '.7*nA * i/3'
     spikes = SpikeMonitor(group); M = StateMonitor(group, 'v', record = 0)
     S = StateMonitor(group, 'v', record = 1); I_m = StateMonitor(group, 'v', record = 2)
     run(duration)
@@ -31,8 +33,8 @@ def hh(duration = 2*second, plots = True):
     if plots:
         subplot(1,2,1); plot(group.I/nA, spikes.count / duration, "k.-", linewidth = .5) # Current vs. FR, 1 datum per neuron
         xlabel('Input Current (nA)'); ylabel('Firing rate (Hz)')
-        subplot(1,2,2); plot(M.t/ms, M.v[0], "k-", linewidth = .5)
+        subplot(1,2,2); plot(M.t/ms, M.v[0], "g--", linewidth = .5)
         plot(S.t/ms, S.v[0], "r--", linewidth = .5); plot(I_m.t/ms, I_m.v[0], "b--", linewidth = .5)
         xlabel('Time (ms)'); ylabel('Voltage (mV)')
         subplots_adjust(wspace=.5); show()
-hh(2*second)
+hh(10*second)
