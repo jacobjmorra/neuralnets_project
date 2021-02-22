fe8d991a6779b24d73449a6261904e4a27993588
jacobjmorra <jacobjmorra@gmail.com>
2021-02-22 00:03:11 -0500

    added preamble

--- single_neuron/code_final/group_2-midterm_project_code.py
+++ single_neuron/code_final/group_2-midterm_project_code.py
@@ -15,7 +15,19 @@ neuronal motifs,” Phys. Rev. E Stat. Nonlin. Soft Matter Phys., vol. 84, no. 2
 from brian2 import *
 
 def MSI(gA, gG):
-    start_scope(); area = 20000*umetre**2
+    """
+    This function initializes and runs a brian2 simulation which is based off of brian2's Hodgkin-Huxley model (please see
+    https://brian2.readthedocs.io/en/stable/examples/IF_curve_Hodgkin_Huxley.html); from here 3 neurons and their synapses
+    are created (1 master, 1 slave, 1 interneuron) --- with excitatory conductance gA and inhibitory conductance gG. A
+    constant current of 320 pA is initialized, the simulation is run for 500 ms, and all spike trains are returned.
+    :param gA: excitatory conductance (allocated as the excitatory weight)
+    :param gG: inhibitory conductance (allocated as the inhibitory weight)
+    :return: spiketrains for M, S, and I neurons in the MSI configuration
+    """
+
+    start_scope()
+
+    area = 20000*umetre**2
 
     # Parameters
     Cm = 1*ufarad*cm**-2 * area
@@ -26,6 +38,7 @@ def MSI(gA, gG):
     Ee, Ei = 0*mV, -80*mV # Synaptic potentials
     taue, taui = 5*ms, 10*ms # Synapse time constants
 
+    #from the brian2 HH example: # https://brian2.readthedocs.io/en/stable/examples/IF_curve_Hodgkin_Huxley.html
     eqs = Equations('''
     dv/dt = (g_na*(m**3)*h*(ENa-v) + g_kd*(n**4)*(EK-v) + gl*(El-v) + ge*(Ee-v) + gi*(Ei-v) + Iext)/Cm : volt
     dm/dt = 0.32*(mV**-1)*4*mV/exprel((13.*mV-v+VT)/(4*mV))/ms*(1-m)-0.28*(mV**-1)*5*mV/exprel((v-VT-40.*mV)/(5*mV))/ms*m : 1
@@ -36,41 +49,35 @@ def MSI(gA, gG):
     Iext : amp
     ''')
 
+    #Create the MIS neurons and initialize voltage and external current parameters
     group = NeuronGroup(3, eqs, threshold='v > -40*mV', refractory='v > -40*mV', method='exponential_euler')
-    group.v = El; group.Iext = ".32*nA" # Initializing voltage and external current
+    group.v = El; group.Iext = ".32*nA"
 
-    # Creating spike monitor, voltage monitors, and synapses
-    spikes = SpikeMonitor(group)
+    # Create state monitors, spike monitors, synapses
     M = StateMonitor(group, 'v', record = 0) # Master
     S = StateMonitor(group, 'v', record = 1) # Slave
     I = StateMonitor(group, 'v', record = 2) # Inter
+
     MSSI = Synapses(group, group, 'w: siemens', on_pre='ge += w_e'); MSSI.connect(i = [0,1], j = [1, 2])
     IS = Synapses(group, group, 'w: siemens', on_pre='gi += w_i'); IS.connect(i = [2], j = [1])
 
-    M_spikes = SpikeMonitor(group, record=True)
-    S_spikes = SpikeMonitor(group, record=True)
-    I_spikes = SpikeMonitor(group, record=True)
+    MSI_spikes = SpikeMonitor(group, record=True)
 
     run(500 * ms)
-    # print("\nFiring Rates (M/S/I):\t\t%s" % spikes.count / duration)
+    #print("\nFiring Rates (M/S/I):\t\t%s" % spikes.count / duration)
 
 
+
+    """
+    #import matplotlib.pyplot as plt
     #plot(M.t/ms, M.v[0], "k-", linewidth = .5)
     #plot(S.t/ms, S.v[0], "r-", linewidth = .5, alpha = .8)
     #plot(I.t/ms, I.v[0], "g--", linewidth = .5, alpha = .8)
     #xlabel('Time (ms)'); ylabel('Voltage (mV)'); show()
     #plt.show()
+    """
 
-    #print(get_diff(M.t, S.t))
-    #print(M.t)
-    #print(S.t)
-    #print(I.t)
-
-    #print("SPIKES,", M_spikes.spike_trains())
-
-    #print(get_diff(M.t, I.t))
-
-    return M_spikes.spike_trains()[0], M_spikes.spike_trains()[1], M_spikes.spike_trains()[2]
+    return MSI_spikes.spike_trains()[0], MSI_spikes.spike_trains()[1], MSI_spikes.spike_trains()[2]
     #print(group.spikes)
     #return(M_spikes.spike_trains()[0], M.v[0], S.t, S.v[0], I.t, I.v[0])
 
